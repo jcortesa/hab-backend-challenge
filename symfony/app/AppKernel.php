110be6bc7f6c4b004744d81b11b2dfd16a41f3ee
Jorge Cortés <jcortesanguita@gmail.com>
2017-10-01 02:16:57 +0200

    refactor: code clean up

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
-            new AppBundle\AppBundle(),
             new HabApiBundle\HabApiBundle(),
         ];
 
